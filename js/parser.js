56a717b4c187cef5a47568f208684925fd0900c7
stormsntides <stormsntides@gmail.com>
2019-08-23 13:00:39 -0400

    Move to text path for displaying SVG tab-notation text.

--- js/parser.js
+++ js/parser.js
@@ -1,3 +1,5 @@
+const ACTIVE_IDS = [];
+
 function parseTabs(text, charSize=5.5, lineSpacing=12){
   // tokenize text and create array to generate string tabs into
 	let tokens = tokenize(text);
@@ -6,14 +8,18 @@ function parseTabs(text, charSize=5.5, lineSpacing=12){
 	let svg = "<svg width='200em' height='20em' viewbox='0 0 1000 100' version='1.1' xmlns='http://www.w3.org/2000/svg'>";
 	svg += "<rect fill='white' x='0' y='0' width='1000' height='100'/>";
 
-	// let tablature = [];
-	let tabString = "<text x='20'>";
-
-	// TODO convert all text to text path
-	let prevStringWrite = 0;
+	// set up path for adding path data to later
+	let pathID = generateRandomId();
+	let pathString = "<path id='tab-path-" + pathID + "' fill='transparent' d='m " + (charSize * 4) + " " + lineSpacing + " ";
+	let tabString = "<text><textPath xmlns:xlink='http://www.w3.org/1999/xlink' xlink:href='#tab-path-" + pathID + "'>";
 
   // currentIndex will hold the indexes of which strings are being written to
   let currentIndex = [];
+	// prevAnchorIndex determines how to move up and down the strings based on last position
+	let prevAnchorIndex = 1;
+	// holds the total size of all chars in the current writing positions string
+	let pathLengthBuff = 0;
+	
 	let options = {
 		showBeats: false,
 		palmMute: false,
@@ -32,18 +38,15 @@ function parseTabs(text, charSize=5.5, lineSpacing=12){
 				let stringCount = 0;
 				while ((buff = re.exec(tkn.value)) !== null) {
 					// get appropriate positioning based on line spacing size
-					// let yPos = (tablature.length + 1) * lineSpacing;
 					let yPos = (stringCount + 1) * lineSpacing;
 
 					lines += " h 2000 m -2000 " + lineSpacing;
 					notes += "<tspan x='0' y='" + yPos + "'>" + buff[0] + "</tspan>";
-					// tablature.push("<tspan x='20' y='" + yPos + "'>");
+
 					stringCount++;
 				}
 				lines += "'/>";
 
-				// size the notes display according to how many notes there are and the line spacing
-				// notes = "<g><rect fill='white' x='0' y='0' width='15' height='" + ((tablature.length + 1) * lineSpacing) + "'/><text>" + notes + "</text><line stroke='black' stroke-width='1' x1='15' y1='0' x2='15' y2='" + ((tablature.length + 1) * lineSpacing) + "'/></g>";
 				notes = "<g><rect fill='white' x='0' y='0' width='15' height='" + ((stringCount + 1) * lineSpacing) + "'/><text>" + notes + "</text><line stroke='black' stroke-width='1' x1='15' y1='0' x2='15' y2='" + ((stringCount + 1) * lineSpacing) + "'/></g>";
 
 				// lines need to go before the notes for proper displaying
@@ -61,7 +64,7 @@ function parseTabs(text, charSize=5.5, lineSpacing=12){
       case "String Chord":
 				currentIndex = [];
         for(let n = 0; n < tkn.value.length; n++){
-					currentIndex.push(tkn.value[n] - 1);
+					currentIndex.push(tkn.value[n]);
         }
         break;
 			case "Percussion":
@@ -108,22 +111,16 @@ function parseTabs(text, charSize=5.5, lineSpacing=12){
     }
   }
 
-	// svg += "<g><text>";
-	// for(let i = 0; i < tablature.length; i++){
-	// 	svg += tablature[i] + "</tspan>";
-	// }
-	// svg += "</text></g></svg>";
+	tabString += "</textPath></text>";
+	pathString += "h " + pathLengthBuff + "'/>";
 
-	// alt text here:
-	svg += "<g>" + tabString + "</g></svg>";
+	svg += "<g>" + pathString + tabString + "</g></svg>";
 
 	return svg;
 
 	function addColumn(char){
-		// for(let st = 0; st < tablature.length; st++){
-		// 	tablature[st] += char;
-		// }
 		tabString += char;
+		pathLengthBuff += (char === "&nbsp;" ? charSize : charSize * char.length);
 	}
 
 	function addTabs(tabToken){
@@ -136,22 +133,49 @@ function parseTabs(text, charSize=5.5, lineSpacing=12){
 
 		let numDigits = 0;
 		tabs.forEach(function(v){ numDigits = numDigits < v.length ? v.length : numDigits; });
+
 		// loop through all notes that are being written to
 		for(let t = 0; t < currentIndex.length; t++){
 			// maxT denotes the max value t can be when get token values
 			let maxT = t >= tabs.length ? tabs.length - 1 : t;
-			// maxT signals to write the last tab to be written on the remaining notes queued
-			// tablature[currentIndex[t]] += "&nbsp;".repeat(numDigits - tabs[maxT].length) + tabs[maxT];
+			tabString += "&nbsp;".repeat(numDigits - tabs[maxT].length) + tabs[maxT];
 
+			// get the x and y positions to adjust the path depending on chords; increase length based on char amount and size
 			let xPos = ((t <= 1 ? t : 1) * -(charSize)) * numDigits;
-			let yPos = (currentIndex[t] + 1) * lineSpacing;
-			tabString += "<tspan dx='" + xPos + "' y='" + yPos + "'>" + "&nbsp;".repeat(numDigits - tabs[maxT].length) + tabs[maxT] + "</tspan>";
+			let yPos = (currentIndex[t] * lineSpacing) - (prevAnchorIndex * lineSpacing);
+			let length = numDigits * charSize;
+
+			// if no change in x or y, buffer length
+			if(xPos === 0 && yPos === 0){
+				pathLengthBuff += length;
+			} else {
+				// if change in x or y, check length buffer and add if greater than 0, then change writing position; reset buffer to new length
+				if(pathLengthBuff > 0){
+					pathString += "h " + pathLengthBuff + " ";
+				}
+				pathString += "m " + xPos + " " + yPos + " ";
+				pathLengthBuff = length;
+			}
+
+			// set previous anchor index to determine which y direction to move on next tab if string change occurs
+			prevAnchorIndex = currentIndex[t];
 		}
-		// fill remaining strings with correct spacing
-		// for(let st = 0; st < tablature.length; st++){
-		// 	if(!currentIndex.includes(st)){
-		// 		tablature[st] += "&nbsp;".repeat(numDigits);
-		// 	}
-		// }
+	}
+
+	function generateRandomId(){
+		let vals = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+		let hash = "";
+		let unique = false;
+
+		while(!unique){
+			for(let i = 0; i < 4; i++){
+				let ri = Math.floor(Math.random() * vals.length);
+				hash += vals[ri];
+			}
+			unique = !ACTIVE_IDS.includes(hash);
+		}
+
+		ACTIVE_IDS.push(hash);
+		return hash;
 	}
 }
